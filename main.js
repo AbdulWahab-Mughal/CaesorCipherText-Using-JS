955c2fca55af24a2a0e0df579eab16150845cff5
AbdulWahab-Mughal <wahaba374@gmail.com>
2023-08-04 09:34:58 -0700

    changes

--- main.js
+++ main.js
@@ -23,32 +23,64 @@
 // }
 
 // }
+// // console.log(convert);
+// let input=prompt("Enter a Message");
+// console.log(input);
+// let convert="";
+// let charCode="";
+// for(let i=0;i<=input.length;i++){
+//     if(input[i]>="A" && input[i]<="Z"){
+//          charCode=((input[i].charCodeAt(0)+65-3)%26)+65;
+//          convert+=String.fromCharCode(charCode)
+//     }
+//     else if(input[i]>="d" && input[i]<="z"){
+//         charCode=((input[i].charCodeAt(0)-97-3)%26)+97;
+//         convert+=String.fromCharCode(charCode)
+
+//    }
+//    else if(input[i]>="a" && input[i]<="c"){
+//     charCode=((input[i].charCodeAt(0)%26)+4)+97
+//     convert+=String.fromCharCode(charCode)
+
+// }
+//    else if(input[i]==" "){
+//     convert+=" ";
+
+// }
+// else if(input[i]>="0" && input[i]<="9"){
+//     charCode=((input[i].charCodeAt(0)-48) %10)+48;
+//     convert+=String.fromCharCode(charCode)
+    
+// }
+
+
+// }
 // console.log(convert);
+
 function Cipher(){
     let plain=document.getElementById("plain").value;
     let convert="";
     let charCode="";
     for(let i=0;i<=plain.length;i++){
-    // let char=input[i];
+    
     if(plain[i]>="A" && plain[i]<="Z"){
-         charCode=((plain[i].charCodeAt(0)-65 +3) %26)+65;
+         charCode=((plain[i].charCodeAt(0)-65 +3)%26)+65;
          convert+=String.fromCharCode(charCode)
          document.getElementById("cipher").innerHTML=convert;
          
     }
     else if(plain[i]>="a" && plain[i]<="z"){
-        charCode=((plain[i].charCodeAt(0)-97 +3) %26)+97;
+        charCode=((plain[i].charCodeAt(0)-97 +3)%26)+97;
         convert+=String.fromCharCode(charCode)
         document.getElementById("cipher").innerHTML=convert;
    }
    else if(plain[i]==" "){
-    // charCode=((plain[i].charCodeAt(0)-97 +3) %26)+97;
-    // convert+=String.fromCharCode(charCode)
+    
     convert+=" ";
     document.getElementById("cipher").innerHTML=convert;
 }
 else if(plain[i]>="0" && plain[i]<="9"){
-    charCode=((plain[i].charCodeAt(0)-48) %10)+48;
+    charCode=((plain[i].charCodeAt(0)-48))+48;
     convert+=String.fromCharCode(charCode)
     document.getElementById("cipher").innerHTML=convert;
 }
